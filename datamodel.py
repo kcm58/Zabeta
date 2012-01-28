aaabe7b64bfa4502ba17b220abefee678a79b205
Michael Brooks <firealwaysworks@gmail.com>
2012-01-28 12:57:07 -0700

    cleanup,  added data model.

---
+++ datamodel.py
@@ -0,0 +1,27 @@
+from google.appengine.ext import db
+
+class outcome(db.Model):
+  description = db.StringProperty()
+  section = db.FloatProperty()
+  
+class course(db.Model):
+  number = db.IntegerProperty()
+  subject = db.StringProperty()
+  term = db.StringProperty()
+  
+class assessment(db.Model):
+  description = db.StringProperty()
+  whenUtilized =  db.StringProperty()
+  outcomes = db.ListProperty(db.ReferenceProperty)
+  direct = db.BooleanProperty()
+
+class measure(db.Model):
+  description = db.StringProperty()
+
+class student(db.Model):
+  name = db.StringProperty()
+  email = db.EmailProperty()
+  
+class faculty(db.Model):
+  name = db.StringProperty()
+  email = db.EmailProperty()
