2e00c6f4fa9440095f39d8e6340c60471bea6cfd
Michael Brooks <firealwaysworks@gmail.com>
2012-02-02 15:16:04 -0700

    new datamodel,  adding poly models.

--- datamodel.py
+++ datamodel.py
@@ -3,38 +3,54 @@ from google.appengine.ext import db
 class course(db.Model):
   number = db.IntegerProperty()
   subject = db.StringProperty()
-  term = db.StringProperty()
-  #A faculty reference
-  instructor = db.ReferenceProperty()
   assessments = db.ListProperty(db.ReferenceProperty())
 
+#Assign course_instance as a child of the proper course.
+class course_instance(db.Model):
+  term = db.StringProperty()
+  year = db.StringProperty()
+  abet_status = db.StringProperty()
+  #A faculty reference
+  instructor = db.ReferenceProperty()
+    
 class assessment(db.Model):
   description = db.StringProperty()
   whenUtilized =  db.StringProperty()
+  #direct/indirect
   direct = db.BooleanProperty()
+  
+class objective(db.Model):
+  description = db.StringProperty()
   outcomes = db.ListProperty(db.ReferenceProperty())
-  survays = db.ListProperty(db.ReferenceProperty())
-  rubrics = db.ListProperty(db.ReferenceProperty())
-
+  
 class outcome(db.Model):
+  section = db.FloatProperty()
   summary = db.StringProperty()
   description = db.StringProperty()
-  section = db.FloatProperty()
   goals = db.ListProperty(db.StringProperty())
+  assessments = db.ListProperty(db.ReferenceProperty())
 
-class survay(db.Model):
+class measure(db.polymodel):
+  description = db.StringProperty()
+  
+class survay(measure):
   description = db.StringProperty()
 
-class rubric(db.Model):
+class rubric(measure):
   description = db.StringProperty()
 
-class measure(db.Model):
+class CourseImprovementDocument(measure):
   description = db.StringProperty()
 
-class student(db.Model):
+class user(db.polymodel):
   name = db.StringProperty()
   email = db.EmailProperty()
   
-class faculty(db.Model):
-  name = db.StringProperty()
-  email = db.EmailProperty()
+class student(user):
+  priviledge = db.StringProperty(default="student")
+
+class faculty(user):
+  priviledge = db.StringProperty(default="faculty")
+
+class admin(user):
+  priviledge = db.StringProperty(default="admin")
