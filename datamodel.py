d78a65babea6e2cf251a5e799cffa5d2a00c7502
Michael Brooks <firealwaysworks@gmail.com>
2012-02-07 13:36:22 -0700

    Implamented session management, working with CAS.

--- datamodel.py
+++ datamodel.py
@@ -1,56 +1,50 @@
 from google.appengine.ext import db
   
-class course(db.Model):
-  number = db.IntegerProperty()
-  subject = db.StringProperty()
-  assessments = db.ListProperty(db.ReferenceProperty())
-
 #Assign course_instance as a child of the proper course.
-class course_instance(db.Model):
-  term = db.StringProperty()
-  year = db.StringProperty()
-  abet_status = db.StringProperty()
-  #A faculty reference
-  instructor = db.ReferenceProperty()
-    
-class assessment(db.Model):
-  description = db.StringProperty()
-  whenUtilized =  db.StringProperty()
-  #direct/indirect
-  direct = db.BooleanProperty()
-  
-class objective(db.Model):
-  description = db.StringProperty()
-  outcomes = db.ListProperty(db.ReferenceProperty())
-  
-class outcome(db.Model):
-  section = db.FloatProperty()
-  summary = db.StringProperty()
-  description = db.StringProperty()
-  goals = db.ListProperty(db.StringProperty())
-  assessments = db.ListProperty(db.ReferenceProperty())
+#class course_instance(db.Model):
+#  term = db.StringProperty()
+#  year = db.StringProperty()
+#  abet_status = db.StringProperty()
+#  #A faculty reference
+#  instructor = db.ReferenceProperty()
+#    
+#class assessment(db.Model):
+#  description = db.StringProperty()
+#  whenUtilized =  db.StringProperty()
+#  #direct/indirect
+#  direct = db.BooleanProperty()
+#  
+#class course(db.Model):
+#  number = db.IntegerProperty()
+#  subject = db.StringProperty()
+#  assessments = db.ListProperty(db.ReferenceProperty(assessment)
+#  
+#  
+#class objective(db.Model):
+#  description = db.StringProperty()
+#  outcomes = db.ListProperty(db.ReferenceProperty())
+#  
+#class outcome(db.Model):
+#  section = db.FloatProperty()
+#  summary = db.StringProperty()
+#  description = db.StringProperty()
+#  goals = db.ListProperty(db.StringProperty())
+#  assessments = db.ListProperty(db.ReferenceProperty())
+#
+#class measure(db.polymodel):
+#  description = db.StringProperty()
+#  
+#class survay(measure):
+#  description = db.StringProperty()
+#
+#class rubric(measure):
+#  description = db.StringProperty()
+#
+#class CourseImprovementDocument(measure):
+#  description = db.StringProperty()
 
-class measure(db.polymodel):
-  description = db.StringProperty()
-  
-class survay(measure):
-  description = db.StringProperty()
-
-class rubric(measure):
-  description = db.StringProperty()
-
-class CourseImprovementDocument(measure):
-  description = db.StringProperty()
-
-class user(db.polymodel):
+class user(db.Model):
   name = db.StringProperty()
   email = db.EmailProperty()
-  
-class student(user):
-  priviledge = db.StringProperty(default="student")
-
-class faculty(user):
-  priviledge = db.StringProperty(default="faculty")
-
-class admin(user):
-  priviledge = db.StringProperty(default="admin")
+  oauth_id = db.StringProperty()
+  cas_id = db.StringProperty()
--- dispatch.py
+++ dispatch.py
@@ -1,12 +1,50 @@
 import json
 import pycas
 import logging
+import Cookie
+import os
+import binascii
+import datamodel
 
-from google.appengine.ext import webapp
+from google.appengine.ext import webapp, db
 from google.appengine.ext.webapp.util import run_wsgi_app
-from google.appengine.api import oauth
+from google.appengine.api import memcache
 
-class dispatch(webapp.RequestHandler):
+class session(webapp.RequestHandler):
+    user_id=0
+    
+    def __init__(self, name, bases):
+        super(session, self).__init__(name, bases)
+        #u=datamodel.user(name="test",email="test@test.com",cas_id="rmb237")
+        #u.put()
+        if "cid" in self.request.cookies:
+            cookie=self.request.cookies["cid"]
+            user_id=memcache.get(cookie)
+            #Is this session active?
+            if user_id:
+                self.user_id=user_id
+                #Reset the server-side timeout value for this session.
+                memcache.add(cookie,self.user_id,7200)
+                self.response.out.write("works:"+str(self.request.cookies["cid"]))
+                #webapp.Request.cookies["cid"]
+            else:
+                self.response.out.write("Doesn't work:"+str(self.request.cookies["cid"]))
+        else:
+            self.new_session("1")
+
+    def new_session(self, user_id):
+        cookie=self.rand()
+        #expires in two hours,  time in seconds. 
+        memcache.add(cookie,user_id,7200)
+        self.response.headers.add_header("Set-Cookie", "cid="+cookie)
+
+    #generate a simple Cryptographic Nonce that is binary safe.
+    def rand(self):
+        r=os.urandom(24)
+        r=binascii.hexlify(r)
+        return r
+
+class dispatch(session):
 
     def get(self):
         self.dispatch()
@@ -40,29 +78,40 @@ class dispatch(webapp.RequestHandler):
         self.response.headers['Content-Type'] = 'application/json'
         self.response.out.write(json_obj)
 
-class index(webapp.RequestHandler):
+class index(session):
 
-    def get(self):
+    def get(self):    
         index=open("client/index.html").read()
         self.response.out.write(index)
         pass
 
-class cas(webapp.RequestHandler):
+class cas(session):
 
     def get(self):
         CAS_SERVER  = "https://cas.nau.edu"
-        SERVICE_URL = "http://localhost:8080/cas/"
+        SERVICE_URL = "http://localhost:9999/cas/"
         status, id, cookie = pycas.login(CAS_SERVER, SERVICE_URL)
-        self.response.out.write("This DOES work<br />Status: "+str(status)+"<br/>UID: "+str(id)+"<br />Cookie:"+str(cookie))
-        
-class o_auth(webapp.RequestHandler):
+        if id:
+          u=db.GqlQuery("select * from user where cas_id=:1",id)
+          user=u.fetch(1)
+          if len(user):
+            user=user[0]
+            self.response.out.write("User logged in:<br />Name: "+str(user.name)+"<br/>UID: "+str(id)+"<br />Cookie:"+str(cookie))
+            self.new_session(user.key())
+          else:
+            self.response.out.write("user not found,  new account?<br />Name: "+str(user.name)+"<br/>UID: "+str(id)+"<br />Cookie:"+str(cookie))
+            #insert a new user:
+            #datamodel.user(name=user.name,email="",cas_id=id).put()
+            #ask the user for their email?
+
+class o_auth(session):
 
     def get(self):
         self.response.out.write("This doesn't work yet!")
-            
+        
         
 
-if __name__ == "__main__":
+if __name__ == "__main__":    
     run_wsgi_app(webapp.WSGIApplication([('/', index),
                                          ('/cas/', cas),
                                          ('/oauth/', o_auth),
