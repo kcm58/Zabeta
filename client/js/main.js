af5e3980cc51d56b7132beb1b1a35f5fcec407cc
Jonah Hirsch <crazydog115@gmail.com>
2012-02-16 13:32:54 -0700

    Started front-end dev

--- client/js/main.js
+++ client/js/main.js
@@ -2,3 +2,60 @@
  * main.js
  */
 
+$(document).ready(function(){
+	loadBackbone();
+	//initUniPicker();
+});
+
+function loadBackbone(){
+	var Zabeta = Backbone.Controller.extend({
+		api_url: "/api/",
+		
+		routes: {
+		"*universityChooser":
+		},
+		
+		defaultAction: function(universityChooser){
+			if(universityChooser){
+				var universityLoaderUrl = this.api_url+'University/list'
+				
+				this.loadData(universityLoaderUrl);
+			}
+		},
+		loadData: function(pageUrl){
+			
+		}
+	});
+	
+	
+}
+
+/*function initUniPicker(){
+	
+	var UniPicker = Backbone.View.extend({
+		el: $('#main'),
+		
+		initialize: function(){
+			//getJSON
+			var json = {"universities":[{"uni_id":"1", "uni_name":"NAU"},{"uni_id":"2", "uni_name":"ASU"}]};
+			_.bindAll(this, 'render');
+			this.render(json);
+		},
+		
+		render: function(json){
+			loadUniPicker(json);
+		}
+	});
+	
+	var uniPicker = new UniPicker();
+	 
+}
+
+function loadUniPicker(json){
+	//alert(json);
+	var src = $('#uni-template').html();
+	var tmpl = Handlebars.compile(src);
+	var html = tmpl(json);
+	$('#main').html(html);
+	
+}*/
