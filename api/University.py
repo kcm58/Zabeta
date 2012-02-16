0e8af80af8fe26b077a4a61fb268bb9d2b9e096b
Michael Brooks <firealwaysworks@gmail.com>
2012-02-16 13:13:39 -0700

    Added university list and crud interface for it.

---
+++ api/University.py
@@ -0,0 +1,14 @@
+import api
+from google.appengine.ext import db
+
+class University(api.api):
+    public={"list":True}
+
+    def list(self):
+        ret=[]
+        u=db.GqlQuery("select * from University")
+        u_list=u.fetch(2048)
+        for i in u_list:
+            ret.append({"id":i.key().__str__() ,
+                        "name":i.name})
+        return ret
